1c4cf87f2dce8e9c8bde81f328cf08b7d050f407
topus009 <sj_89@mail.ru>
2019-11-10 15:53:38 +0300

    Updates

---
+++ precache-manifest.7f0b200055de5c17e3d4f89b0693ed82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48580f8254ed512a4faba42308600a9",
+    "url": "/green-todo/index.html"
+  },
+  {
+    "revision": "7f2c4c13740436a662d5",
+    "url": "/green-todo/static/css/main.1d0e3a1f.chunk.css"
+  },
+  {
+    "revision": "a67c5998d8253804bb6d",
+    "url": "/green-todo/static/js/2.349c84b7.chunk.js"
+  },
+  {
+    "revision": "c9a87b2d086404328f22",
+    "url": "/green-todo/static/js/3.96f6dd50.chunk.js"
+  },
+  {
+    "revision": "5697195e497d9ea0a287",
+    "url": "/green-todo/static/js/4.3b86b8ad.chunk.js"
+  },
+  {
+    "revision": "9f0aa39b84a5f342437d",
+    "url": "/green-todo/static/js/5.b36cf994.chunk.js"
+  },
+  {
+    "revision": "7f2c4c13740436a662d5",
+    "url": "/green-todo/static/js/main.9a0b566f.chunk.js"
+  },
+  {
+    "revision": "ca74cb43e66a59e73114",
+    "url": "/green-todo/static/js/runtime-main.9e11b7e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/green-todo/precache-manifest.b3acc4daf900b02b6408bc874873b04d.js"
+  "/green-todo/precache-manifest.7f0b200055de5c17e3d4f89b0693ed82.js"
 );
 
 self.addEventListener('message', (event) => {
