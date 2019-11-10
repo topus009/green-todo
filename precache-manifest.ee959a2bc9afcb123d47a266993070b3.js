82f67578417db2ffafc3ee3c51b88ee6e46a86bd
topus009 <sj_89@mail.ru>
2019-11-10 15:37:24 +0300

    Updates

---
+++ precache-manifest.ee959a2bc9afcb123d47a266993070b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c4a1478d60cde89438ebef4fd53443",
+    "url": "/green-todo/index.html"
+  },
+  {
+    "revision": "1a89064454505a0214bd",
+    "url": "/green-todo/static/css/main.e739f57b.chunk.css"
+  },
+  {
+    "revision": "1a6aefa0346cf81dc942",
+    "url": "/green-todo/static/js/2.2f34a924.chunk.js"
+  },
+  {
+    "revision": "45788830aba2f27587d0",
+    "url": "/green-todo/static/js/3.b5c419e1.chunk.js"
+  },
+  {
+    "revision": "5b56532837306a710ef1",
+    "url": "/green-todo/static/js/4.77031b22.chunk.js"
+  },
+  {
+    "revision": "262842399901c4006d48",
+    "url": "/green-todo/static/js/5.ea1cd4f2.chunk.js"
+  },
+  {
+    "revision": "1a89064454505a0214bd",
+    "url": "/green-todo/static/js/main.d06d1177.chunk.js"
+  },
+  {
+    "revision": "494d92390d704cf6d655",
+    "url": "/green-todo/static/js/runtime-main.a0fa22c4.js"
+  }
+]);
